0739a572f7c2777974222eb6f125030b8f89b47d
Tomasz Janczuk <tomasz@janczuk.org>
2012-02-02 13:37:21 -0800

    add support for multiple ports to nlb

--- src/nlb.js
+++ src/nlb.js
@@ -1,31 +1,31 @@
 var config = require('./nlb.json');
 var net = require('net');
 
-var currentBackend = 0;
-
 console.log('NLB: TCP level round robin load balancer')
-console.log('Listen port: ' + config.port);
-console.log('Backends:');
-config.backends.forEach(function (backend) { console.log('    ' + backend.host + ':' + backend.port)});
+console.log('Ctrl-C to terminate');
 
-function routeToBackend(connection) {
-	var backend = config.backends[currentBackend];
-	currentBackend++;
-	currentBackend %= config.backends.length;
+function routeToBackend(connection, route) {
+	var backend = route.backends[currentBackend];
+	route.currentBackend++;
+	route.currentBackend %= route.backends.length;
 	var client = net.connect(backend.port, backend.host, function () {
-		console.log('Routing new connection to ' + backend.host + ':' + backend.port);
+		console.log('Routing new connection on port ' + route.port + ' to ' + backend.host + ':' + backend.port);
 		connection.resume();
 		connection.pipe(client).pipe(connection);
 	}).on('error', function (err) {
-		console.log('Error routing new connection to ' + backend.host + ':' + backend.port + ': ' + err);
+		console.log('Error routing new connection on port ' + route.port + ' to ' + backend.host + ':' + backend.port + ': ' + err);
 		connection.end();
 	});
 }
 
-net.createServer(function (connection) {
-	connection.pause();
-	routeToBackend(connection);
-}).listen(config.port, function() {
-	console.log('NLB is bound to the listen port');
-	console.log('Ctrl-C to terminate');
-})
+config.forEach(function (route) {
+	route.currentBackend = 0;
+	net.createServer(function (connection) {
+		connection.pause();
+		routeToBackend(connection, route);
+	}).listen(route.port, function() {
+		console.log('NLB is bound to the listen port ' + route.port);
+		console.log('  Backends:');
+		route.backends.forEach(function (backend) { console.log('    ' + backend.host + ':' + backend.port)});
+	})	
+});
